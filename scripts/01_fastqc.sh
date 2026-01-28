e7c59be831fd6f2c74cb09159a4a878c3ba18976
Jason Greene <jason.greene.22@mail.dcu.ie>
2026-01-28 17:13:56 +0000

    Clean up FastQC script and add threading support

--- scripts/01_fastqc.sh
+++ scripts/01_fastqc.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Run FastQC on all gzipped FASTQ files
-# Output goes to FastQC directory
+#Use threads (t -) ,where t =  number of CPU threads (i.e. how fast you want to run!) 
+#Output goes to FastQC directory
 
 mkdir -p FastQC
-cd~/FastQC
-fastqc *.fastq.gz --outdir FastQC
+cd ~/FastQC
+fastqc -t 4 *.fastq.gz --outdir FastQC
 
-#To run FastQC on multiple files in a directroy use threads (t -) ,where t = number of FastQ  files you want to run
-fastqc -t 4 *.fastq.gz --outdir 
 
 #To assess and interpret your FastQC results use this link to help understand  
 #https://hbctraining.github.io/Intro-to-rnaseq-hpc-salmon-flipped/lessons/07_qc_fastqc_assessment.html
