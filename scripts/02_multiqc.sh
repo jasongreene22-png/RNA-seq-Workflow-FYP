a291cf7f696759bdbc968c60755d50a2b24628fd
Jason Greene <jason.greene.22@mail.dcu.ie>
2026-01-29 15:10:20 +0000

    Merge FastQC and MultiQC scripts into single QC step

--- scripts/02_multiqc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#MultiQC gives will provide easy to interpret data about your raw sequences by giving reports on collection of sample files
-# to run multiqc on a directory containing fastqc reports for example run 
-mutliqc ~/FastQc
-
-#For more info on how to interpret MultiQC reports visit https://multiqc.info/ 
-#To find out what version of multiqc you are using 
-multiqc --version 
