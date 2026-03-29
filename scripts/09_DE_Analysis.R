25661586b044a138d3ccd93d1dd3d64d9db83718
jasongreene22-png <jason.greene22@mail.dcu.ie>
2026-03-29 22:45:01 +0100

    Implement differential expression analysis for R47H vs WT
    Added differential expression analysis and visualization for R47H vs WT comparison, including heatmap and volcano plot.

--- scripts/09_DE_Analysis.R
+++ scripts/09_DE_Analysis.R
@@ -28,3 +28,47 @@ pheatmap(sampleDistMatrix,
          clustering_distance_rows = sampleDists,
          clustering_distance_cols = sampleDists,
          col = colors)
+
+# Run the differential expression pipeline on the raw counts
+dds <- DESeq(dds)
+R47HvsWT <-results(dds, contrast=c("Genotype","R47H","WT"), independentFiltering = TRUE , alpha = 0.05)
+summary(R47HvsWT)
+head(R47HvsWT)
+
+table(R47HvsWT$padj < 0.05)
+R47HvsWT[order(R47HvsWT$padj),] %>% head
+
+plotCounts(dds, gene="TREM2", intgroup="genotype") 
+
+write.table(as.data.frame(R47HvsWT[order(R47HvsWT$padj),] ), file="R47H_vs_WT_dge_study1.txt",sep="\t", quote = FALSE)
+
+
+# Plot Heatmap of the Top 20 DE genes 
+R47HvsWT_sorted <- R47HvsWT[order(R47HvsWT$padj),]
+R47HvsWT_sorted
+R47HvsWT_top20 <- head(R47HvsWT_sorted, n=20)
+top20_genes_R47HvsWT <- rownames(R47HvsWT_top20)
+top20_genes_R47HvsWT
+
+rlog.dge <- rld[top20_genes_R47HvsWT, ] %>% assay
+
+pheatmap(rlog.dge, scale="row", main = "Differential Gene Expression for Study1 (row-based z-score)")
+
+R47HvsWT_tb <- R47HvsWT %>% data.frame() %>% rownames_to_column(var="gene") %>% as_tibble()
+
+R47HvsWT_tb <- R47HvsWT_tb %>% mutate(threshold = padj < 0.05)
+R47HvsWT_tb <- R47HvsWT_tb %>% arrange(padj) %>% mutate(genelabels = "")
+R47HvsWT_tb$genelabels[1:10] <- R47HvsWT_tb $gene[1:10]
+
+
+
+volcano1<- ggplot(R47HvsWT_tb, aes(x = log2FoldChange, y = -log10(padj))) +
+  geom_point(aes(colour = threshold)) +
+  geom_text_repel(aes(label = genelabels)) +
+  ggtitle("R47H vs WT DE gene expression Study 1") +
+  xlab("log2 fold change") + 
+  ylab("-log10 adjusted p-value") +
+  theme(legend.position = "none",
+        plot.title = element_text(size = rel(1.5), hjust = 0.5),
+        axis.title = element_text(size = rel(1.25))) 
+
